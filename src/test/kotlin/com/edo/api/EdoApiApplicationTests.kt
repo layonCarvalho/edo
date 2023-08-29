84c9ab68dce87116eec90d6a8481977794d87f75
layonCarvalho <layon.300@hotmail.com>
2023-08-29 16:16:55 -0300

    Start

---
+++ src/test/kotlin/com/edo/api/EdoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.edo.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdoApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
