b5f0a030b3206f86ee25acfe6a4234448cb0a9c1
danmacedoo <270412015@eniac.edu.br>
2023-09-28 08:45:56 -0300

    Order, Stock, Product

---
+++ src/test/kotlin/com/edo/api/order/service/OrderServiceImplTest.kt
@@ -0,0 +1,12 @@
+package com.edo.api.order.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class OrderServiceImplTest {
+
+    @Test
+    fun placeOrder() {
+    }
+}
---
+++ src/test/kotlin/com/edo/api/product/service/ProductServiceImplTest.kt
@@ -0,0 +1,36 @@
+package com.edo.api.product.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ProductServiceImplTest {
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun getAll() {
+    }
+
+    @Test
+    fun getById() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun delete() {
+    }
+
+    @Test
+    fun getProductById() {
+    }
+
+    @Test
+    fun updateProduct() {
+    }
+}
